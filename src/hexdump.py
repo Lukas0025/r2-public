fd823d16e129cd2f58c918665cb77b4035e29394
Lukáš Plevač <lukasplevac@gmail.com>
2021-02-10 17:30:51 +0100

    Update hexdump.py

--- src/hexdump.py
+++ src/hexdump.py
@@ -1,18 +1,19 @@
-def hexdump(src, length=16, sep='.', lines_num=None):
-  FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or sep for x in range(256)])
-  lines = []
-  count = 0
-  for c in range(0, len(src), length):
-    chars = src[c:c+length]
-    hexstr = ' '.join(["%02x" % ord(x) for x in chars]) if type(chars) is str else ' '.join(['{:02x}'.format(x) for x in chars])
-    if len(hexstr) > 24:
-      hexstr = "%s %s" % (hexstr[:24], hexstr[24:])
-    printable = ''.join(["%s" % ((ord(x) <= 127 and FILTER[ord(x)]) or sep) for x in chars]) if type(chars) is str else ''.join(['{}'.format((x <= 127 and FILTER[x]) or sep) for x in chars])
-    lines.append("<tr><td>%08x</td><td>%-*s</td><td>%s</td></tr>" % (c, length*3, hexstr, printable))
-    
-    count += 1
-    if (lines_num is not None) and (count >= lines_num):
-        lines.append("<tr><td>Rows deleted. Rows limit is %s</td></tr>" % (lines_num))
-        break
+# from: https://github.com/ActiveState/code/blob/master/recipes/Python/142812_Hex_dumper/recipe-142812.py
+# Under MIT
 
-  return '\n'.join(lines)
+FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
+
+def dump(src, length=16, bytes_lim=None):
+    N=0; result=''
+    while src:
+       s,src = src[:length],src[length:]
+       hexa = ' '.join(["<td>%02X</td>"%ord(x) for x in s])
+       s = s.translate(FILTER)
+       result += "<tr><td>%04X</td>%-*s<td>%s</td></tr>\n" % (N, length*3, hexa, s)
+       N+=length
+       
+       if byte_lim < N:
+           result += "<tr>Bytes removed - over limit %s</tr>\n" % (N)
+           break
+
+    return result
