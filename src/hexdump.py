2d6d53fc04a7b90c94873ac81c0f583398b477e3
Lukáš Plevač <lukasplevac@gmail.com>
2021-02-10 17:44:14 +0100

    Update hexdump.py

--- src/hexdump.py
+++ src/hexdump.py
@@ -7,12 +7,12 @@ def hexdump(src, length=16, bytes_lim=None):
     N=0; result=''
     while src:
        s,src = src[:length],src[length:]
-       hexa = ' '.join(["<td>%02X</td>"%ord(x) for x in s])
+       hexa = ' '.join(["<td>%02X</td>"% x for x in s])
        s = s.translate(FILTER)
        result += "<tr><td>%04X</td>%-*s<td>%s</td></tr>\n" % (N, length*3, hexa, s)
        N+=length
        
-       if byte_lim < N:
+       if not(bytes_lim is None) and byte_lim < N:
            result += "<tr>Bytes removed - over limit %s</tr>\n" % (N)
            break
 
