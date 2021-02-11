b343f9ef4c624520013f5f26c1baa7ec5fe234f7
Lukáš Plevač <lukasplevac@gmail.com>
2021-02-11 09:36:56 +0100

    Update hexdump.py

--- src/hexdump.py
+++ src/hexdump.py
@@ -1,13 +1,25 @@
 # from: https://github.com/ActiveState/code/blob/master/recipes/Python/142812_Hex_dumper/recipe-142812.py
 # Under MIT
 
+def getAsciiColor(char_num):
+  if (char_num < 33) or (char_num > 127):
+    return "#4040a0"
+  elif (char_num >= 33) and (char_num <= 47):
+    return "#239123"
+  elif (char_num >= 48) and (char_num <= 57):
+    return "#962d96"
+  elif ((char_num >= 58) and (char_num <= 59)) or ((char_num >= 91) and (char_num <= 96)) or ((char_num >= 123) and (char_num <= 126)):
+    return "#118888"
+  elif ((char_num >= 65) and (char_num <= 90)) or ((char_num >= 97) and (char_num <= 122)):
+    return "#b92d2d"
+  
 def hexdump(src, length=16, bytes_lim=None):
   sep = '.'
   FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or sep for x in range(256)])
   lines = []
   for c in range(0, len(src), length):
     chars = src[c:c+length]
-    hexstr = ''.join(["<td>%02x</td>" % ord(x) for x in chars]) if type(chars) is str else ''.join(['<td>{:02x}</td>'.format(x) for x in chars])
+    hexstr = ''.join(["<td style='color:%s'>%02x</td>" % (getAsciiColor(ord(x)), ord(x)) for x in chars]) if type(chars) is str else ''.join(['<td style="color:%s">{:02x}</td>'.format(getAsciiColor(ord(x)), x) for x in chars])
     printable = ''.join(["%s" % ((ord(x) <= 127 and FILTER[ord(x)]) or sep) for x in chars]) if type(chars) is str else ''.join(['{}'.format((x <= 127 and FILTER[x]) or sep) for x in chars])
     lines.append("<tr><td>%08x</td>%-*s<td>%s</td></tr>" % (c, length*3, hexstr, printable))
     
