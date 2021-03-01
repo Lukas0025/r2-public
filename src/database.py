f6b53c8f4dfcc0675252471b89cae08e7ec80c31
Lukáš Plevač <lukasplevac@gmail.com>
2021-03-01 11:12:51 +0000

    database documentation

--- src/database.py
+++ src/database.py
@@ -3,6 +3,11 @@ import json
 
 class database:
     
+    ## database object conscructor
+    # create conection to database
+    # @param self
+    # @param config - named array with config [host, port, user, pass, db]
+    # @return DB obj
     def __init__(self, config):
         self.link = mysql.connector.connect(
             host=config['host'],
@@ -14,15 +19,25 @@ class database:
         
         self.cursor = self.link.cursor(buffered=True, dictionary=True)
             
-            
+    
+    ## database object descructor
+    # close conection to database
+    # @param self
+    # @return none
     def __del__(self): 
         self.close()
-        
+    
+    ## check is databse inited
+    # @param self
+    # @return bool
     def isInited(self):
         self.cursor.execute("SHOW TABLES LIKE 'observations'")
         
         return self.cursor.fetchone()
-        
+    
+    ## init database schema
+    # @param self
+    # @return none
     def initDb(self):
         self.cursor.execute("""DROP TABLE IF EXISTS `observations`;
                                                 CREATE TABLE `observations` (
@@ -45,6 +60,10 @@ class database:
                                                          PRIMARY KEY(`id`)
                                                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;""")
 
+    ## insert observation to database
+    # @param self
+    # @param ob observation object - from r2server.observation
+    # @return none
     def insertObservation(self, ob):
         if ob.haveA:
             aUrl = ob.aURL
@@ -100,12 +119,19 @@ class database:
 
         self.cursor.execute(sql, val)
 
+    ## check is observation with ID exist
+    # @param self
+    # @param obid int - id of oesrvation - r2server.observation.id
+    # @return bool
     def haveObservation(self, obid):
         sql = "SELECT id FROM observations WHERE id = %s"
         val = (obid,)
         self.cursor.execute(sql, val)
         return self.cursor.rowcount > 0
 
+    ## load list of ground stations
+    # @param self
+    # @return list of json.loads ground station
     def getGroundStations(self):
         sql = "SELECT groundStation FROM observations GROUP BY groundStation"
         self.cursor.execute(sql)
@@ -117,56 +143,98 @@ class database:
 
         return out
 
+    ## load list of observations with data
+    # @param self
+    # @param limit int - limit of observations
+    # @return list of json.loads ground station
     def getObservationsWithData(self, limit):
         sql = "SELECT * FROM observations WHERE aUrl is not NULL OR dataUrl is not NULL ORDER BY start DESC LIMIT %s"
         val = (limit,)
         self.cursor.execute(sql, val)
         return self.cursor.fetchall()
 
+    ## get last observation with A channel
+    # @param self
+    # @return named array of observation
     def getLastWithA(self):
         sql = "SELECT * FROM observations WHERE aUrl is not NULL ORDER BY start DESC LIMIT 1"
         self.cursor.execute(sql)
         return self.cursor.fetchall()[0]
 
-
+    ## get observation with id
+    # @param self
+    # @param id int - r2server.observation.id
+    # @return named array of observation
     def getObservation(self, id):
         sql = "SELECT * FROM observations WHERE id = %s"
         val = (id,)
         self.cursor.execute(sql, val)
         return self.cursor.fetchall()[0]
 
+    ## get list of observations ORDER BY start time
+    # @param self
+    # @param id int - r2server.observation.id
+    # @return named array of observation
     def observationList(self, limit):
         sql = "SELECT * FROM observations ORDER BY start DESC LIMIT %s"
         val = (limit,)
         self.cursor.execute(sql, val)
         return self.cursor.fetchall()
 
+    ## get list of observations by satellite with norad
+    # @param self
+    # @param norad int - narad id of satellite
+    # @param limit int - max len of array
+    # @return named array of observation
     def observationListOfSat(self, norad, limit):
         sql = "SELECT * FROM observations WHERE norad = %s ORDER BY start DESC LIMIT %s"
         val = (norad, limit,)
         self.cursor.execute(sql, val)
         return self.cursor.fetchall()
 
+    ## get list of observations by ground station
+    # @param self
+    # @param ground string - json.dumps of groundstation
+    # @param limit int - max len of array
+    # @return named array of observation
     def observationListOfGroundStation(self, ground, limit):
         sql = "SELECT * FROM observations WHERE groundStation = %s ORDER BY start DESC LIMIT %s"
         val = (ground, limit,)
         self.cursor.execute(sql, val)
         return self.cursor.fetchall()
 
+    ## get list of observations in specific date
+    # @param self
+    # @param time string - string of date
+    # @return named array of observation
     def observationListInTime(self, time):    
         self.cursor.execute('SELECT * FROM observations WHERE DATEDIFF(start, %s) = 0 ORDER BY start DESC', (time,))
         return self.cursor.fetchall()
 
+    ## get list of expired observations (to delete over TTL)
+    # @param self
+    # @param ttl int - number of days to live
+    # @return named array of observation
     def expiredObservationList(self, ttl):
         self.cursor.execute('SELECT * FROM observations WHERE DATEDIFF(NOW(), start) > %s ORDER BY start DESC', (ttl,))
         return self.cursor.fetchall()
 
+    ## remove observation from database
+    # @param self
+    # @param obid int - observation id - r2server.observation.id
+    # @return none
     def removeObservation(self, obid):
         self.cursor.execute('DELETE FROM observations WHERE id = %s', (obid,))
 
+    ## commit changes in DB
+    # @param self
+    # @return none
     def commit(self):
         self.link.commit()
 
+    ## close connection to DB
+    # @param self
+    # @return none
     def close(self):
         self.cursor.close()
         self.link.close()
